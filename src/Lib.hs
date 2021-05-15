d0c8326721bbf940ef80fb8c1756095d2c408013
Leon Nikitin <nlv@lab321.ru>
2021-05-16 01:32:29 +0600

    Пример создания простого документа

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,21 @@ module Lib
     ( someFunc
     ) where
 
+import Text.XML.HXT.Core
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  runX $ root [] [helloWorld] >>> writeDocument [withIndent yes] "hello.xml"
+  return ()
+
+helloWorld :: ArrowXml a => a XmlTree XmlTree
+helloWorld
+    = mkelem "html" []
+        [ mkelem "head" []
+          [ mkelem "title" []
+            [ txt "Hello World" ]     
+          ]
+        , mkelem "body"
+            [ sattr "class" "haskell" ]
+            [ mkelem "h1" [] [ txt "Hello World" ] ]
+        ]  
