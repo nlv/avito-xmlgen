f0f05a23318bd9c92b92c71529eb71a1508f497f
Leon Nikitin <nlv@lab321.ru>
2022-01-30 00:12:49 +0600

    Генератор возвращает String. Вывод в самой функции main

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,13 @@ module Main where
 
 import Lib
 
+import Options.Applicative
+
 main :: IO ()
-main = someFunc
+main = 
+  let ops = argument str (help "URL of data") in do
+  src <- execParser $ info (ops <**> helper) (fullDesc <> progDesc "Converting Avito posts" <> Options.Applicative.header "header")
+  res <- generateXML src
+  case res of
+    Left err -> putStrLn err
+    Right xml -> putStr xml
