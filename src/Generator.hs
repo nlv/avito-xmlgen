6cfd351777d2a04238a29806ce9367a2c6533211
Leon Nikitin <nlv@lab321.ru>
2022-07-30 22:33:17 +0600

    Универсалный генератор. Обрабатывает простые поля, desciprion, images, address

--- src/Generator.hs
+++ src/Generator.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE  DeriveGeneric, OverloadedStrings, FlexibleInstances, RecordWildCards #-}
+{-# LANGUAGE  DeriveGeneric, OverloadedStrings, FlexibleInstances, RecordWildCards, TupleSections #-}
 
 module Generator
     ( generateXML
@@ -8,7 +8,10 @@ import GHC.Generics
 
 import Control.Applicative
 import Control.Monad (mzero, guard)
-import qualified Data.ByteString.Lazy as BL 
+-- import qualified Data.ByteString.Lazy as BL -- (fromStrict, toStrict)
+import qualified Data.ByteString.Lazy as BLL -- (fromStrict, toStrict)
+import qualified Data.ByteString as BL -- (fromStrict, toStrict)
+-- import qualified Data.ByteString.Char8 as BL 
 import Data.List as L
 import Data.List.Utils (replace)
 import Data.Csv
@@ -35,57 +38,6 @@ import Data.List.Split as Split
 import Text.XML.HXT.Core
 import qualified Text.XML.HXT.DOM.XmlNode as Node
 
-data Ad = Ad { adId :: !String
-             , adDateBegin :: !String
-             , adDateEnd :: !String
-             , adStatus :: !String
-             , adAllowEmail :: !String
-             , adManagerName :: !String
-             , adContactPhone :: !String
-             , adAddress :: !String
-             , adCategory :: !String
-             , adCondition :: !String
-             , adGoodsType :: !String
-             , adGoodsSubType :: !String
-             , adType :: !String
-             , adTitle :: !String
-             , adDescription :: !String
-             , adPrice :: !String
-             , adVideoURL :: !String
-             , adImages :: ![String]
-             } deriving Generic
-
-
-instance FromNamedRecord Ad where
-  parseNamedRecord r = 
-      Ad <$> r .: "Id" 
-         <*> (trimString <$> (r .: "DateBegin"  <|> pure ""))
-         <*> (trimString <$> (r .: "DateEnd"  <|> pure ""))
-         <*> (r .: "AdStatus" <|> pure "") 
-         <*> (r .: "AllowEmail" <|> pure "")
-         <*> (r .: "ManagerName" <|> pure "") 
-         <*> r .: "ContactPhone" 
-         <*> ((L.intercalate ", ") <$> L.filter (not . L.null) <$> sequenceA (L.map (r .:) address))
-         <*> r .: "Category" 
-         <*> (r .: "Condition" <|> pure "") 
-         <*> r .: "GoodsType" 
-         <*> r .: "GoodsSubType" 
-         <*> r .: "AdType" 
-         <*> r .: "Title" 
-         <*> (fmap descriptionHtml (r .: "Description"))
-         <*> r .: "Price" 
-         <*> r .: "VideoURL" 
-         <*> ((Split.splitOn ",") <$> r .:  "ImageNames")
-      where address = ["addrRegion", "addrArea", "addrCity", "addrPoint", "addrStreet", "addrHouse"]
-            -- descriptionHtml d = either (unpack . renderError) id $ runPure (descriptionHtml2 d)
-            descriptionHtml d = either (const "Ошибка в описании") id $ runPure (descriptionHtml2 d)
-            descriptionHtml2 d = do
-              d1 <- readMarkdown def (pack d)
-              d2 <- writeHtml5String def d1
-              return $ unpack d2            
-
-trimString = L.dropWhileEnd isSpace . L.dropWhile isSpace
- 
 
 generateXML :: Text -> IO (Either String String)
 generateXML src = 
@@ -97,14 +49,16 @@ generateXML src =
         Left err -> pure $ Left err
         Right csvData ->
           let l = encodeUtf8 $ T.unlines $ L.drop 0 $ T.lines $ decodeUtf8 csvData in
-          case decodeByName (BL.fromStrict l) of
+          case decode NoHeader (BLL.fromStrict l) of
             Left err -> pure $ Left err
-            Right (_, v) -> do
-              [res] <- runX $ root [] [makeAds (V.toList v)] >>> writeDocumentToString [withRemoveWS yes]
+            Right v -> do
+              let v' = mkMap $ L.map V.toList $ V.toList v
+              [res] <- runX $ root [] [makeAds v'] >>> writeDocumentToString [withRemoveWS yes]
               pure $ Right res
-              -- pure $ Right $ decodeUtf8 $ BL.toStrict res
-              -- [res] <- runX $ root [] [makeAds (V.toList v)] >>> writeDocument [withIndent yes] "Ads.xml"
-              -- pure $ Right $ Node.toText res
+
+  where  mkMap :: [[BL.ByteString]] -> [[(BL.ByteString, BL.ByteString)]]
+         mkMap (h : rs) = L.map ((L.filter (\(h',_) -> h' /= "")) . L.zip h) rs
+         mkMap _        = []
 
 makeGoogleExportCSVURI :: Text -> Maybe String
 makeGoogleExportCSVURI x = maybe Nothing (Just . renderStr) ((mkURI x) >>= convertURI)
@@ -122,52 +76,88 @@ makeGoogleExportCSVURI x = maybe Nothing (Just . renderStr) ((mkURI x) >>= conve
                                                              uriQuery = formatParam : gidParam : qs, uriFragment = Nothing}
            return p'''
 
-makeAd :: ArrowXml a => Ad -> a XmlTree XmlTree
-makeAd ad@(Ad {adGoodsType = "Вакансии", ..}) = 
-        mkelem "Ad" []
-          [ mkelem "Id" [] [ txt (adId) ]
-          , mkelem "DateBegin" [] [ txt (adDateBegin) ]
-          , mkelem "DateEnd" [] [ txt (adDateEnd) ]
-          , mkelem "Status" [] [ txt (adStatus) ]
-          , mkelem "AllowEmail" [] [ txt (adAllowEmail) ]
-          , mkelem "ManagerName" [] [ txt (adManagerName) ]
-          , mkelem "ContactPhone" [] [ txt (adContactPhone) ]
-          , mkelem "Address" [] [ txt (adAddress) ]
-          , mkelem "Category" [] [ txt (adGoodsType ad) ]
-          , mkelem "Condition" [] [ txt (adCondition) ]
-          , mkelem "Industry" [] [ txt (adGoodsSubType) ]
-          , mkelem "AdType" [] [ txt (adType) ]
-          , mkelem "Title" [] [ txt (adTitle) ]
-          , mkelem "Description" [] [ constA (adDescription) >>> mkCdata ]
-          , mkelem "Salary" [] [ txt (adPrice) ]
-          , mkelem "VideoURL" [] [ txt (adVideoURL) ]
-          , mkelem "Images" [] $ images (adImages)
-          ]
-makeAd ad = 
-        mkelem "Ad" []
-          [ mkelem "Id" [] [ txt (adId ad) ]
-          , mkelem "DateBegin" [] [ txt (adDateBegin ad) ]
-          , mkelem "DateEnd" [] [ txt (adDateEnd ad) ]
-          , mkelem "Status" [] [ txt (adStatus ad) ]
-          , mkelem "AllowEmail" [] [ txt (adAllowEmail ad) ]
-          , mkelem "ManagerName" [] [ txt (adManagerName ad) ]
-          , mkelem "ContactPhone" [] [ txt (adContactPhone ad) ]
-          , mkelem "Address" [] [ txt (adAddress ad) ]
-          , mkelem "Category" [] [ txt (adCategory ad) ]
-          , mkelem "Condition" [] [ txt (adCondition ad) ]
-          , mkelem "GoodsType" [] [ txt (adGoodsType ad) ]
-          , mkelem "GoodsSubType" [] [ txt (adGoodsSubType ad) ]
-          , mkelem "AdType" [] [ txt (adType ad) ]
-          , mkelem "Title" [] [ txt (adTitle ad) ]
-          , mkelem "Description" [] [ constA (adDescription ad) >>> mkCdata ]
-          , mkelem "Price" [] [ txt (adPrice ad) ]
-          , mkelem "VideoURL" [] [ txt (adVideoURL ad) ]
-          , mkelem "Images" [] $ images (adImages ad)
-          ]
+makeAd :: ArrowXml a => [(BL.ByteString, BL.ByteString)] -> a XmlTree XmlTree
+makeAd vs = mkelem "Ad" [] $ L.map (makeEl . (dr *** dr)) $ groupAddress $ L.map (dc *** dc) vs
+  where dr = L.dropWhileEnd isSpace . L.dropWhile isSpace
+        dc = T.unpack . decodeUtf8
 
-images :: ArrowXml a => [String] -> [a XmlTree XmlTree]
-images is = L.map (\i -> mkelem "Image" [ sattr "url" i ] []) $ (L.take 8) is
+makeEl :: ArrowXml a => (String, String) -> a XmlTree XmlTree
+makeEl ("ImageNames", v) = 
+  mkelem "Images" [] $ 
+         L.map (\i -> mkelem "Image" [ sattr "url" i ] []) $ Split.splitOn ","  v
 
-makeAds :: ArrowXml a => [Ad] -> a XmlTree XmlTree
+makeEl ("Description", v) = 
+  mkelem "Description" [] [constA ( descriptionHtml v) >>> mkCdata]
+  where
+    descriptionHtml d = either (const "Ошибка в описании") id $ runPure (descriptionHtml2 d)
+    descriptionHtml2 d = do
+      d1 <- readMarkdown def (pack d)
+      d2 <- writeHtml5String def d1
+      return $ unpack d2
+
+makeEl (n, v) = mkelem n [] [ txt v]
+
+
+groupAddress :: [(String, String)] -> [(String, String)]
+groupAddress = uncurry (:) . ((("Address",) . L.intercalate ", " . sortOn o . L.filter (/= "") . (L.map snd)) *** id) .  L.partition ((`elem` addrElems) . fst)
+  where addrElems = ["addrRegion", "addrArea", "addrCity", "addrPoint", "addrStreet", "addrHouse"]
+        o "addrRegion" = 1
+        o "addrArea"   = 2
+        o "addrCity"   = 3
+        o "addrPoint"  = 4
+        o "addrStreet" = 5
+        o "addrHouse"  = 6
+        o _            = 7
+
+
+
+
+-- makeAd :: ArrowXml a => Ad -> a XmlTree XmlTree
+-- makeAd ad@(Ad {adGoodsType = "Вакансии", ..}) = 
+--         mkelem "Ad" []
+--           [ mkelem "Id" [] [ txt (adId) ]
+--           , mkelem "DateBegin" [] [ txt (adDateBegin) ]
+--           , mkelem "DateEnd" [] [ txt (adDateEnd) ]
+--           , mkelem "Status" [] [ txt (adStatus) ]
+--           , mkelem "AllowEmail" [] [ txt (adAllowEmail) ]
+--           , mkelem "ManagerName" [] [ txt (adManagerName) ]
+--           , mkelem "ContactPhone" [] [ txt (adContactPhone) ]
+--           , mkelem "Address" [] [ txt (adAddress) ]
+--           , mkelem "Category" [] [ txt (adGoodsType ad) ]
+--           , mkelem "Condition" [] [ txt (adCondition) ]
+--           , mkelem "Industry" [] [ txt (adGoodsSubType) ]
+--           , mkelem "AdType" [] [ txt (adType) ]
+--           , mkelem "Title" [] [ txt (adTitle) ]
+--           , mkelem "Description" [] [ constA (adDescription) >>> mkCdata ]
+--           , mkelem "Salary" [] [ txt (adPrice) ]
+--           , mkelem "VideoURL" [] [ txt (adVideoURL) ]
+--           , mkelem "Images" [] $ images (adImages)
+--           ]
+-- makeAd ad = 
+--         mkelem "Ad" []
+--           [ mkelem "Id" [] [ txt (adId ad) ]
+--           , mkelem "DateBegin" [] [ txt (adDateBegin ad) ]
+--           , mkelem "DateEnd" [] [ txt (adDateEnd ad) ]
+--           , mkelem "Status" [] [ txt (adStatus ad) ]
+--           , mkelem "AllowEmail" [] [ txt (adAllowEmail ad) ]
+--           , mkelem "ManagerName" [] [ txt (adManagerName ad) ]
+--           , mkelem "ContactPhone" [] [ txt (adContactPhone ad) ]
+--           , mkelem "Address" [] [ txt (adAddress ad) ]
+--           , mkelem "Category" [] [ txt (adCategory ad) ]
+--           , mkelem "Condition" [] [ txt (adCondition ad) ]
+--           , mkelem "GoodsType" [] [ txt (adGoodsType ad) ]
+--           , mkelem "GoodsSubType" [] [ txt (adGoodsSubType ad) ]
+--           , mkelem "AdType" [] [ txt (adType ad) ]
+--           , mkelem "Title" [] [ txt (adTitle ad) ]
+--           , mkelem "Description" [] [ constA (adDescription ad) >>> mkCdata ]
+--           , mkelem "Price" [] [ txt (adPrice ad) ]
+--           , mkelem "VideoURL" [] [ txt (adVideoURL ad) ]
+--           , mkelem "Images" [] $ images (adImages ad)
+--           ]
+
+-- images :: ArrowXml a => [String] -> [a XmlTree XmlTree]
+-- images is = L.map (\i -> mkelem "Image" [ sattr "url" i ] []) $ (L.take 8) is
+
+makeAds :: ArrowXml a => [[(BL.ByteString, BL.ByteString)]] -> a XmlTree XmlTree
 makeAds as
     = mkelem "Ads" [ sattr "formatVersion" "3", sattr "target" "Avito.ru" ] (L.map makeAd as)
